ea8a6d5a60b70764316706a885a9f27ab8806e47
xuxa <gui.soliveiras@gmail.com>
2024-06-07 15:58:39 -0300

    feat: see profile image on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import { getServerSession } from 'next-auth'
+import Image from 'next/image'
 import LinkShortnerForms from './components/LinkShortnerForms'
 
 export default async function Home() {
-  // const session = await getServerSession()
-  // console.log(session?.user)
+  const session = await getServerSession()
 
   return (
     <div>
+      <h1>Olá {session?.user?.name}</h1>
+      <Image
+        src={session?.user?.image || ''}
+        alt="profile-image"
+        width={50}
+        height={50}
+      />
       <LinkShortnerForms />
     </div>
   )
